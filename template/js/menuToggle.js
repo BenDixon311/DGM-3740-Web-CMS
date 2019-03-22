9ae069ebb4858302be08738ee8af0c949034e48d
Ben Dixon <bendixon311@gmail.com>
2019-03-21 22:13:48 -0600

    updated

---
+++ template/js/menuToggle.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+function toggleMenu() {
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+var x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu; 
