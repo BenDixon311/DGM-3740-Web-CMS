4bdb89b1541ffdd6f454b6d637f2177949735d94
Ben Dixon <bendixon311@gmail.com>
2019-03-23 15:46:54 -0600

    updated

--- phpsite/cruises.php
+++ phpsite/cruises.php
@@ -1,10 +1,27 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php  
+	$title = "Cruises";
+	include '_top.php';
+?>
 
-<body>
-</body>
-</html>
+<div class="cruise">
+	<h1>Cruises</h1>
+	
+	<div class = "cruiseList">
+
+			<img src = "images/cruiseship1.jpg">
+		<div class = "cruiseDesc">
+			<h2>Billy Bob's Cruise</h2>
+			<p class="cruiseInfo">Billy Bob will take you on a grand adventure you won't regret. Well mostly. Be prepared to see the greatest sites on the crappiest boat. Good luck.</p>
+			<button type = "button" class = "readMore">Read More</button>
+		</div>
+		
+	</div>
+
+	
+	
+
+</div>
+
+	<?php include 
+	'_bot.php';
+?>
