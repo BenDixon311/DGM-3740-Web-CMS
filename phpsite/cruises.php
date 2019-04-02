63919d3a9a310bb7958ca15bc5e24ad62dce7b37
Ben Dixon <bendixon311@gmail.com>
2019-04-02 15:45:25 -0600

    updated latest

--- phpsite/cruises.php
+++ phpsite/cruises.php
@@ -8,12 +8,30 @@
 	
 	<div class = "cruiseList">
 
-			<img src = "images/cruiseship1.jpg">
-		<div class = "cruiseDesc">
-			<h2>Billy Bob's Cruise</h2>
+		<img class= "cruiseImg" src = "images/cruiseship1.jpg">
+		<figure class = "cruiseDesc">
+			<h2>Billy Bob's Cruise from $85!</h2>
 			<p class="cruiseInfo">Billy Bob will take you on a grand adventure you won't regret. Well mostly. Be prepared to see the greatest sites on the crappiest boat. Good luck.</p>
-			<button type = "button" class = "readMore">Read More</button>
-		</div>
+			<button type = "button" class = "readMore">READ MORE</button>
+		</figure>
+		
+		
+	</div>
+	
+		<div class = "cruiseList">
+
+		<img class= "cruiseImg" src = "images/cruiseship2.jpg">
+		<figure class = "cruiseDesc">
+			<h2>Elegant Journey Cruise from $999!</h2>
+			<p class="cruiseInfo">On the other side of the world, Australia and New
+				Zealand are full of wonders to behold! Now, more
+				than ever before, you can get phenomenal
+				discounts to cruise the South Pacific. Sail to New
+				Zealand from Sydney, Australia on a 12-day cruise
+				from just $999 per person plus taxes</p>
+			<button type = "button" class = "readMore">READ MORE</button>
+		</figure>
+		
 		
 	</div>
 
