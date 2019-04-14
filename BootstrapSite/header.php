46c444ae002dfa867d23547da1d22f54a7d08ed0
Ben Dixon <bendixon311@gmail.com>
2019-04-13 18:53:22 -0600

    big changies

--- BootstrapSite/header.php
+++ BootstrapSite/header.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	  <link rel="stylesheet" href="css/style.css">
+	  <link href="https://fonts.googleapis.com/css?family=Montserrat:700|Playfair+Display:400, 700" rel="stylesheet">
 
     <title><?php echo $title ?></title>
   
@@ -27,14 +28,12 @@
         }
       }
 		
-		body {
-		  padding-top: 3.5rem;
-		}
+		
     </style>
     <!-- Custom styles for this template -->
   </head>
   <body>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background-color:#000000">
+    <nav class="navbar sticky-top navbar-expand-md navbar-dark" style="background-color:#000000">
   <a class="navbar-brand" href="#">
 			<img src="images/goldesignlogosmall.png" alt="Goldesign Logo">
 		</a>
@@ -48,24 +47,16 @@
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Services</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link" href="#">Hand Engraving</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-        <div class="dropdown-menu" aria-labelledby="dropdown01">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Gallery</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+
   </div>
 </nav>
 
--- BootstrapSite/index.php
+++ BootstrapSite/index.php
@@ -5,32 +5,59 @@ $title = "Home";
 
   <!-- Main jumbotron for a primary marketing message or call to action -->
   <div class="jumbotron">
-    <div class="container">
-      <h1 class="display-5 text-light">Fine Jewelry By Design</h1>
-      <p class="text-light">From custom design and repairs, to appraisals and fine hand engraving, we guarantee your satisfaction!</p>
-      <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn More &raquo;</a></p>
+    <div class="container" style="padding-top: 100px;">
+		<center>
+      <h1 class="display-5 text-light" style="letter-spacing: 3px;">GOLDESIGN JEWELERS</h1>
+      <p class="text-light">Fine Jewelery by Design</p>
+      <p><a class="btn btn-secondary btn-md" href="#" role="button">Services</a></p>
+		</center>
     </div>
   </div>
 
   <div class="container">
     <!-- Example row of columns -->
+	  <center>
     <div class="row">
-      <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      <div class="col-xs-12 col-md-4">
+		
+			<div class="card" >
+			  <img class="card-img-top" src="images/rings.jpeg" alt="Card image">
+			  <div class="card-body" style="background-color: black">
+				<h4 class="card-title text-light">Gallery</h4>
+				
+				<a href="#" class="btn btn-secondary stretched-link">See More</a>
+			  </div>
+			</div>	  
+
+		</div>
+		<br>
+      <div class="col-xs-12 col-md-4">
+		<div class="card" >
+		  <img class="card-img-top" src="images/he3.jpg" alt="Card image">
+		  <div class="card-body" style="background-color: black">
+			<h4 class="card-title text-light">Hand Engraving</h4>
+			
+			<a href="#" class="btn btn-secondary stretched-link">See More</a>
+		  </div>
+		</div>
       </div>
-      <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+		<br>
+      <div class="col-xs-12 col-md-4">
+       		<div class="card" >
+		  <img class="card-img-top" src="images/services.jpg" alt="Card image">
+		  <div class="card-body" style="background-color: black">
+			<h4 class="card-title text-light">Services</h4>
+			
+			<a href="#" class="btn btn-secondary stretched-link">See More</a>
+		  </div>
+		</div>
+		  
+		</div>
       </div>
-      <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-      </div>
+		</center>
+		<br>
     </div>
+		  
 
     <hr>
 
